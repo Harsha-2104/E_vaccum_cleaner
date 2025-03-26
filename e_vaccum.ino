5899ed803b635f96308004260d792a38600eb4ad
Harsha-2104 <harshayanapu11@gmail.com>
2025-03-26 16:39:56 +0530

    Update e_vaccum.ino

--- e_vaccum.ino
+++ e_vaccum.ino
@@ -1,29 +1,27 @@
-#include <Servo.h>
 int a1=3;
 int a2=4;
 int b1=5;
 int b2=6;
-//int c1 = 8;
-//int c2 = 7;
+int c1 = 8;
+int c2 = 7;
 char c;
-//int count=1;
+int count=1;
 Servo myservo;
 
 void setup() {
-pinMode(a1,OUTPUT);
-pinMode(a2,OUTPUT);
-pinMode(b1,OUTPUT);
-pinMode(b2,OUTPUT);
-//pinMode(c1,OUTPUT);
-//pinMode(c2,OUTPUT);
+  pinMode(a1,OUTPUT);
+  pinMode(a2,OUTPUT);
+  pinMode(b1,OUTPUT);
+  pinMode(b2,OUTPUT);
+  pinMode(c1,OUTPUT);
+  pinMode(c2,OUTPUT);
   Serial.begin(9600);
   Serial.println("SERIAL COMMUNICATION");
- /*   digitalWrite(a1,0);
+  digitalWrite(a1,0);
   digitalWrite(a2,0);
   digitalWrite(b1,0);
-  digitalWrite(b2,0);*/
-  myservo.attach(9);
-  myservo.write(90);
+  digitalWrite(b2,0);
+
 }
 
 void loop() {
@@ -59,54 +57,16 @@ void loop() {
   digitalWrite(b2,1);
   Serial.println("forward");}
   else if(c=='5'){
-
-    myservo.write(105);
-    delay(70);
-    myservo.write(120);
-    delay(70);
-    myservo.write(135);
-    delay(70);
-    myservo.write(150);
-    delay(70);
-    myservo.write(165);
-    delay(70);
-    myservo.write(180);
-    delay(70);
-    myservo.write(165);
-    delay(70);
-    myservo.write(150);
-    delay(70);
-    myservo.write(135);
-    delay(70);
-    myservo.write(120);
-    delay(70);
-    myservo.write(105);
-    delay(70);
-    myservo.write(90);
-    delay(70);
-    myservo.write(75);
-    delay(70);
-    myservo.write(60);
-    delay(70);
-    myservo.write(45);
-    delay(70);
-    myservo.write(30);
-    delay(70);
-    myservo.write(15);
-    delay(70);
-    myservo.write(0);
-    delay(70);
-    myservo.write(15);
-    delay(70);
-    myservo.write(30);
-    delay(70);
-    myservo.write(45);
-    delay(70);
-    myservo.write(60);
-    delay(70);
-    myservo.write(75);
-    delay(70);
-    myservo.write(90);
+     if(count<2){
+        count += 1;
+        digitalWrite(c1,0);
+        digitalWrite(c2,1);
+     }
+     else if(count == 2){
+       count = 1;
+       digitalWrite(c1,0);
+       digitalWrite(c2,1);      
+     } 
   }
  else {
   digitalWrite(a1,0);
